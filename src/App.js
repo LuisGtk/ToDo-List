769cd8d358cb438c92bccf3de9eebc251c50e4a9
LuisGtk <luisgonzalez189@gmail.com>
2022-08-17 14:33:10 -0400

    adding header css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import ToDoListData from './data/toDoListData'
+import ItemForm from './Components/ItemForm/ItemForm'
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
   return (
     <div className="App">
       <h1>My To Do List:</h1>
-
+      <ItemForm setToDoNewItem={setToDoNewItem} />
     </div>
   );
 }
